478fce2d5f44a88b44aef15f577c909383a601ed
buer1020w <1403624364@qq.com>
2024-05-25 16:29:20 +0800

    提交合并前的本地更改

--- src/router.tsx
+++ src/router.tsx
@@ -3,6 +3,7 @@ import {
 } from 'react-router-dom';
 import App from './App';
 import Page210812000 from "./210812000";
+import Page210812035 from "./210812035";
 
 const router = createBrowserRouter([
   {
@@ -21,6 +22,10 @@ const router = createBrowserRouter([
     path: '210812005',
     element: <div> 210812005 的作业内容</div>,
   },
+  {
+    path: '210812035',
+    element: <Page210812035 />,
+  }
 ]);
 
 export { router };
