25453e412b74e3e915ec90aacf9590dd23187348
lingxiao <3280919460@qq.com>
2024-05-29 01:50:41 +0800

    12更改

--- src/210812012/index.tsx
+++ src/210812012/index.tsx
@@ -1,22 +1,22 @@
-import { useState } from 'react';
-
-
-
 export default () => {
-  const [task, setTask] = useState<any>(null);
   return (
     <div>
       <h1>210812012 华聪瑞的作业内容</h1>
-      <a href='./src/210812012/three-demo/index.html' target='_blank'>
+      <a href='https://github.com/lingxiaojust/lizi.git'>
         粒子加载作业
       </a>
-<br />
+      <br />
+      <a href='https://github.com/lingxiaojust/shader.git'>
+        阴影作业
+      </a>
+      <br />
       <a href='https://github.com/lingxiaojust/-hcr' target='_blank'>
         three.js模型加载作业
-      </a>
-      {/* <a href='https://github.com/MoveLikeRabbit/3DScene' target='_blank'>
+      </a> 
+      <br />
+      <a href='https://github.com/lingxiaojust/HomeWork0525' target='_blank'>
         游戏场景大作业
-      </a> */}
+      </a>
     </div>
   );
 };
