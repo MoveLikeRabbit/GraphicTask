81e2b8dada6131c48d092d8011b57979f99a783e
AkariiiCBI <2465440761@qq.com>
2024-05-29 18:32:09 +0800

    001

--- src/210812023/index.tsx
+++ src/210812023/index.tsx
@@ -6,7 +6,7 @@ export default () => {
     const [task, setTask] = useState<any>(null);
     return (
         <div>
-            <h1>210812023 刘若彤平时作业</h1>
+            <h1>210812023 刘若彤作业</h1>
             <button
                 onClick={() => {
                     setTask('Box');
@@ -15,7 +15,7 @@ export default () => {
                 查看3DBox
             </button>
 
-
+            <br></br>
 
             <button
                 onClick={() => {
@@ -24,15 +24,18 @@ export default () => {
             >
                 查看canvas cube
             </button>
-
+            <br></br>
             <a href='https://github.com/AkariiiCBI/Bird ' target='_blank'>
                 鸟飞行模型
             </a >
-
+                <br></br>
             <a href='https://github.com/AkariiiCBI/shadow-Snowflake-cube ' target='_blank'>
                 阴影、方块、雪花
             </a >
+            <br></br>
+            <a href='https://github.com/lingxiaojust/HomeWork0525' target='_blank'>
             游戏场景大作业
+            </a>
             {task === 'Box' && <Box />}
             {task === 'cube' && <Cube />}
         </div>
